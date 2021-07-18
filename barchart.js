19722fbee79164745781cda58c2a986f660f5030
JuliaDawson <julia.dawson80@gmail.com>
2021-07-18 10:50:31 -0500

    Update barchart.js

--- barchart.js
+++ barchart.js
@@ -1,28 +1,27 @@
-var data = [4,8,15,16,23,42];
-var color = ['pink','lightyellow','lightgreen','lightcyan','lightblue','violet'];
-var pie = d3.pie();
-var arc = d3.arc().innerRadius(0).outerRadius(100);
-var svg = d3.select("svg"),
-        width = svg.attr("width"),
-        height = svg.attr("height"),
-        margin = 50
-        radius = Math.min(width-2*margin, height-2*margin) / 2,
-        g = svg.append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+var margin = 50;
+var width = 300;
+var height = 300;
+var xscale = d3.scaleBand().domain([0,1,2,3,4,5]).range([0,200]);
+var yscale = d3.scaleLinear().domain([0, d3.max(data)]).range([200,0]);
 
-    var color = d3.scaleOrdinal(color);
+var x_axis = d3.axisBottom().scale(xscale);
+var y_axis = d3.axisLeft().scale(yscale);
 
-    // Generate the arcs
-    var arc = d3.arc()
-                .innerRadius(0)
-                .outerRadius(radius);
-
-    //Generate groups
-    var arcs = g.selectAll("path")
-                .data(pie(data))
-                .enter()
-                .append("path")
-                .attr("d", arc)
-                .attr("fill", function(d, i) {
-                     return color(i);
-                 });
+d3.select("svg")
+  .attr("width",width)
+  .attr("height",height)
+  .append("g")
+    .attr("transform","translate("+margin+","+margin+")")
+    .selectAll("rect").data(data).enter().append("rect")
+       .attr("x",function(d,i) { return (33.3333333*i)+0; }) 
+       .attr("y",function(d) { return 200-(200*d/42); })
+       .attr("width",33.3333)
+       .attr("height",function(d) { return (200*d/42); }); 
+d3.select("svg")
+  .attr("width",width+2*margin)
+  .attr("height",height+2*margin)
+  .append("g")
+    .attr("transform", "translate("+margin+","+margin+")").call(y_axis)
+  .append("g")
+    .attr("transform", "translate("+margin+",250)").call(x_axis);
 
