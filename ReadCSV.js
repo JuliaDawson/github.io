6b5cf81bd4fa91b19a92aa6b1a8805414f32a0c2
JuliaDawson <julia.dawson80@gmail.com>
2021-07-19 20:04:14 -0500

    Update ReadCSV.js

--- ReadCSV.js
+++ ReadCSV.js
@@ -1,4 +1,7 @@
-d3.csv("CS416_CountryRegion.csv", function(data) {
+//CS416_CountryRegion.csv.csv
+//CS416_1.3_Distribution_of_income.csv
+//CS416_1.4_Shared_Prosperity.csv
+d3.csv("CS416_1.3_Distribution_of_income.csv", function(data) {
 				console.log(data);
 				
 			});
