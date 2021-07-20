14164d5ef4c4787a4a48df2f74887f7b3c307a1c
JuliaDawson <julia.dawson80@gmail.com>
2021-07-19 19:57:31 -0500

    Update ReadCSV.js

--- ReadCSV.js
+++ ReadCSV.js
@@ -1,6 +1,4 @@
-d3.csv("./CS416_CountryRegion.csv", function(data) {
-    for (var i = 0; i < data.length; i++) {
-        console.log(data[i].Name);
-        console.log(data[i].Age);
-    }
-});
+d3.csv("CS416_CountryRegion.csv", function(data) {
+				console.log(data);
+				
+			});
