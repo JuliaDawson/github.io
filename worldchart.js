1259ba76468e11eafaae904abd4ef33557ed3715
JuliaDawson <julia.dawson80@gmail.com>
2021-07-20 18:28:24 -0500

    Update worldchart.js

--- worldchart.js
+++ worldchart.js
@@ -19,8 +19,6 @@ d3.json('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json')
 
    const countries = topojson.feature(data, data.objects.countries); 
    
-   g.selectAll('path').data(countries.features).enter().append('path').attr('class','country').attr('d', path)
-       .attr('fill','#cccccc')	
-       .attr('stroke','#333333')	
-       .attr('stroke-width', 0.5);	
+   g.selectAll('path').data(countries.features).enter().append('path').attr('class','country').attr('d', path);
+	
   });
