fdfcba82d6f59a3b87cbde8cb6db817cf96031e2
JuliaDawson <julia.dawson80@gmail.com>
2021-07-20 19:33:15 -0500

    Update worldchart.js

--- worldchart.js
+++ worldchart.js
@@ -1,16 +1,15 @@
 const width = 900;
-const height = 600
+const height = 600;
 
 const svg = d3.select('body').append('svg')
                 .attr('width',width)
-				.attr('height',height)
+		.attr('height',height)
 				
 //https://www.youtube.com/watch?v=urfyp-r255A
 //cdn.jsdeliv.net/npm/world-atlas@2/countries-110m.json		
 //github.com/d3/d3-geo
-//https://www.youtube.com/watch?v=aNbgrqRuoiE 25 min
 
-const projection = d3.geoMercator().scale(140).translate([width/2,height/1.4]).scale(100);
+const projection = d3.geoMercator().scale(140).translate([width/2,height/2]).scale(100);
 const path = d3.geoPath(projection);
 
 const g = svg.append('g');
@@ -18,7 +17,17 @@ const g = svg.append('g');
 d3.json('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json')
   .then(data => {
 
-   const countries = topojson.feature(data, data.objects.countries); 
+   var countries = topojson.feature(data, data.objects.countries).features; 
    
-   g.selectAll('path').data(countries.features).enter().append('path').attr('class','country').attr('d', path).attr('fill','#ccc');
+   g.selectAll('.country')
+	   .data(countries)
+	   .enter().append('path')
+	   .attr('class','country')
+	   .attr('d', path)
+	   /* Could replace with mouseover, mouseout, see www.youtube.com watch?v=aNbgrqRuoiE */
+	   .on('click', function(d) {
+	       d3.select(this).classed("selected". true)
+            })
+
+
   });
