8edd3d6f50a7ccd8f4e4ead2ee4746ec4a328427
JuliaDawson <julia.dawson80@gmail.com>
2021-07-20 19:27:10 -0500

    Update worldchart.js

--- worldchart.js
+++ worldchart.js
@@ -9,8 +9,9 @@ const svg = d3.select('body').append('svg')
 //cdn.jsdeliv.net/npm/world-atlas@2/countries-110m.json		
 //github.com/d3/d3-geo
 
-const projection = d3.geoMercator().scale(140).translate([width/2,height/2]).scale(100);
-const path = d3.geoPath(projection);
+var projection = d3.geoMercator().scale(140).translate([width/2,height/2]).scale(100)
+
+var path = d3.geoPath.projection(projection);
 
 const g = svg.append('g');
 		
