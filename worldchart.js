a1f0e4c3e8e672cb132fc6b75b3d0999b6ab602e
JuliaDawson <julia.dawson80@gmail.com>
2021-07-20 19:13:55 -0500

    Update worldchart.js

--- worldchart.js
+++ worldchart.js
@@ -28,8 +28,6 @@ d3.json('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json')
 	   .on('click', function(d) {
 	       d3.select(this).classed("selected". true)
             })
-       .attr('fill','#cccccc')	
-       .attr('stroke','#333333')	
-       .attr('stroke-width', 0.5);	
+
 
   });
