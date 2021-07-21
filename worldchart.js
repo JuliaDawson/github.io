48e96dc34046f7fa285b9db2f5afa4f62e4da242
JuliaDawson <julia.dawson80@gmail.com>
2021-07-20 20:37:07 -0500

    Update worldchart.js

--- worldchart.js
+++ worldchart.js
@@ -28,9 +28,41 @@ d3.json('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json')
 	   .attr('stroke-width', '1')
 	   .attr('d', path)
 	   /* Could replace with mouseover, mouseout, see www.youtube.com watch?v=aNbgrqRuoiE */
-	   .on('click', function(d) {
-	       d3.select(this).classed("selected". true)
-            })
+            .on("mouseover", handleMouseOver)
+            .on("mouseout", handleMouseOut);
+})
 
+ // Create Event Handlers for mouse
+      function handleMouseOver(d, i) {  // Add interactivity
+
+            // Use D3 to select element, change color and size
+            d3.select(this).attr({
+              fill: "orange",
+              // r: radius * 2
+            });
+
+            // Specify where to put label of text
+	    // Create an id for text so we can select it later for removing on mouseout
+            //svg.append("text").attr({
+            //   id: "t" + d.x + "-" + d.y + "-" + i,  
+            //   x: function() { return xScale(d.x) - 30; },
+            //    y: function() { return yScale(d.y) - 15; }
+            //})
+            //.text(function() {
+            //  return [d.x, d.y];  // Value of the text
+            //});
+          }
+
+      function handleMouseOut(d, i) {
+            // Use D3 to select element, change color back to normal
+            d3.select(this).attr({
+              fill: "#cccccc",
+              //r: radius
+            });
+
+            // Select text by id and then remove
+            // d3.select("#t" + d.x + "-" + d.y + "-" + i).remove(); 
+          }
+	
 
   });
