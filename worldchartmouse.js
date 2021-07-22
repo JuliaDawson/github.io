c094a1fe9cd4599971f0ad6de5517e03b0098296
JuliaDawson <julia.dawson80@gmail.com>
2021-07-22 16:17:19 -0500

    Update worldchartmouse.js

--- worldchartmouse.js
+++ worldchartmouse.js
@@ -21,14 +21,26 @@ var svg = d3.select('body')
   var g = svg.append("g");
 
 
-  
-  //add a call to d3.json to load the TopoJSON file
-  //so it loads into our visualization
+// const g = svg.append('g').attr("transform", "translate(50,50)");
+		
 d3.json('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json')
   .then(data => {
-    g.append('path')
-      .datum(topojson.feature(data, data.objects.countries))
-      .attr('d', path);
+
+   var countries = topojson.feature(data, data.objects.countries).features; 
+   
+   g.selectAll('.country')
+	   .data(countries)
+	   .enter().append('path')
+	   .attr('class','country')
+	   .attr('fill', '#cccccc')
+	   .attr('stroke', '#333333') 
+	   .attr('stroke-width', '1')
+	   .attr('d', path)
+	   /* Could replace with mouseover, mouseout, see www.youtube.com watch?v=aNbgrqRuoiE */
+            .on("mouseover", handleMouseOver)
+            .on("mouseout", handleMouseOut);
+})
+
     
     //create the zoom effect
     var zoom = d3.behavior.zoom()
@@ -40,72 +52,20 @@ d3.json('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json')
       });
     svg.call(zoom);
     
-    // Load the data from the json file
-    d3.json('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/meteorite-strike-data.json', function(error, data) {
-      if (error) 
-        console.error(error);
-      
-      var locations = data.features;
-      var hue = 0; //create the circles
-      
-      // we will pass our data (the TopoJSON) as an argument, then create SVG elements using a classic D3 append. Selecting all paths, the TopoJSON is bound in the data method. From here, we can perform work on each element.
-      
-      
-      locations.map(function(d) {  // Create an object for holding dataset
-        hue += 0.36                // Create property for each circle, give it value from color
-        d.color = 'hsl(' + hue + ', 100%, 50%)';
-      });
-      
-      // Classic D3... Select non-existent elements, bind the data, append the elements, and apply attributes
-      g.selectAll('circle')
-        .data(locations)
-        .enter()
-        .append('circle') //show the circles
-        .attr('cx', function(d) {
-          if (d.geometry) {
-            return projection([d.geometry.coordinates[0], d.geometry.coordinates[1]])[0];
+
+ // Create Event Handlers for mouse
+      function handleMouseOver(d, i) {  // Add interactivity
+            // Use D3 to select element, change color and size
+	      console.log("handleMouseIn  ",d, i);
+	      d3.select(this).attr({    
+                fill: "orange"
+              });
           }
-        })
-        .attr('cy', function(d) {
-          if (d.geometry) {
-            return projection([d.geometry.coordinates[0], d.geometry.coordinates[1]])[1];
+
+      function handleMouseOut(d, i) {
+	    console.log("handleMouseOut ",d, i);	
+            // Use D3 to select element, change color back to normal
+            d3.select(this).attr({
+              fill: "#cccccc"
+            });
           }
-        })
-        .attr('r', function(d) {
-          if (d.properties.mass) {
-            return Math.pow(parseInt(d.properties.mass), 1 / 9);
-          }
-        })
-        .style('fill', function(d) {
-        //Use the Color Function to set the Fill Value for each circle
-          return d.color;
-        })
-      
-      //Next, we need to write two pieces of code, one that listens for when the value of the tooltip changes, and one that updates the SVG elements.
-      //We are going to use some D3 code to listen for an input change on the tooltip elements
-      
-      //Add Event Listeners | mouseover
-        .on('mouseover', function(d) {
-          d3.select(this).style('fill', 'black'); 
-          d3.select('#name').text(d.properties.name);
-          d3.select('#nametype').text(d.properties.nametype);
-          d3.select('#fall').text(d.properties.fall);
-          d3.select('#mass').text(d.properties.mass);
-          d3.select('#recclass').text(d.properties.recclass);
-          d3.select('#reclat').text(d.properties.reclat);
-          d3.select('#reclong').text(d.properties.reclong);
-          d3.select('#year').text(d.properties.year);
-          d3.select('#tooltip')
-            .style('left', (d3.event.pageX + 20) + 'px')
-            .style('top', (d3.event.pageY - 80) + 'px')
-            .style('display', 'block')
-            .style('opacity', 0.8)
-        })
-        //Add Event Listeners | mouseout
-        .on('mouseout', function(d) { 
-          d3.select(this).style('fill', d.color);
-          d3.select('#tip')
-            .style('display', 'none');
-        });
-    });
-  });
