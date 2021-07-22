c972696fdf450d3ad7f6b5f90dfaa6b946846ca1
JuliaDawson <julia.dawson80@gmail.com>
2021-07-22 16:13:37 -0500

    Update worldchartmouse.js

--- worldchartmouse.js
+++ worldchartmouse.js
@@ -24,8 +24,8 @@ var svg = d3.select('body')
   
   //add a call to d3.json to load the TopoJSON file
   //so it loads into our visualization
-  d3.json('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json', function(error, data) {
-    if (error) console.error(error);
+d3.json('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json')
+  .then(data => {
     g.append('path')
       .datum(topojson.feature(data, data.objects.countries))
       .attr('d', path);
