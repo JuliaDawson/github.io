26b44067f5abf8fcefd953873e0f81dcf06f7d25
JuliaDawson <julia.dawson80@gmail.com>
2021-07-23 18:46:43 -0500

    Update worldchartmouse.js

--- worldchartmouse.js
+++ worldchartmouse.js
@@ -10,25 +10,17 @@ var margin = 50;
 //https://www.youtube.com/watch?v=dJbpo8R47D0
 var projection = d3.geoMercator().translate([width/2,height/2]).scale(140);
 var path = d3.geoPath().projection(projection);
-
        
-  let svg = d3.select("#visualization").append("svg")
+let svg = d3.select("#visualization").append("svg")
     .attr("width", width + margin + margin)
     .attr("height", height + margin + margin)
     .append("g")
     .attr("transform",
           "translate(" + margin + "," + margin + ")");
                    
-  let div = d3.select("body").append("div")	
+ let div = d3.select("body").append("div")	
     .attr("class", "tooltip")				
     .style("opacity", 0); 
-
-  // Append empty placeholder g element to the SVG
-  // g will contain geometry elements
-  var g = svg.append("g");
-
-
-// const g = svg.append('g').attr("transform", "translate(50,50)");
 		
 d3.json('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json')
   .then(data => {
@@ -45,7 +37,7 @@ d3.json('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json')
 	   .attr('d', path)
 	   /* Could replace with mouseover, mouseout, see www.youtube.com watch?v=aNbgrqRuoiE */
     .on("mouseover", function(d) {
-      console.log("mouseover  ",d.properties.name, d.population);
+      console.log("mouseover  ",d.properties.name);
       d3.select(this).style('fill', 'orange');
       div.html("Country: " + d.properties.name)  
       .style("left", (d3.event.pageX) + "px")		
