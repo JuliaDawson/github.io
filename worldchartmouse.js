1c0fd50e4204466e727fcd90b2b395822e1ef8d4
JuliaDawson <julia.dawson80@gmail.com>
2021-07-24 13:15:12 -0500

    Update worldchartmouse.js

--- worldchartmouse.js
+++ worldchartmouse.js
@@ -11,7 +11,7 @@ d3.csv("MatchTopo_Distribution_of_income_Shared_Prosperity.csv", function(error,
       console.log(data[i].DISPCountry);
       console.log(data[i].DISPRegion);	   
    }	   
-}
+});
 for (i=0;i<dispinfo.length;i++){
     console.log(i,": ", dispinfo.data[i]);    
 }
