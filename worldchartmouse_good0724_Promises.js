4cb611497a860bc670fc2f79e66fd59a29b9442b
JuliaDawson <julia.dawson80@gmail.com>
2021-07-25 03:24:12 -0500

    Update worldchartmouse_good0724_Promises.js

--- worldchartmouse_good0724_Promises.js
+++ worldchartmouse_good0724_Promises.js
@@ -1,19 +1,21 @@
-var width = 1400;
-var height = 700;
-
+var width = 900;
+var height = 600;
 
 //Read CSV and make a hash table
-//d3.csv("MatchTopo_Distribution_of_income_Shared_Prosperity.csv", function(data) {
-//    return {	
-//            countryName: data.DISPCountry,
-//	    region:     data.DISPRegion,
-//	    giniIndex: +data.DIGiniIndex
-//    }	    
-//    //console.log(data);	
-//});					        
+d3.csv("MatchTopo_Distribution_of_income_Shared_Prosperity.csv", function(data) {
+    return {	
+            countryName: data.DISPCountry,
+	    region:     data.DISPRegion,
+	    giniIndex: +data.DIGiniIndex
+    }	    
+    console.log(data);	
+});					        
 
-var econdata = d3.csv("MatchTopo_Distribution_of_income_Shared_Prosperity.csv");
-var worldmap = d3.json('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json')
+
+
+//d3.csv("MatchTopo_Distribution_of_income_Shared_Prosperity.csv", function(data) {
+//				console.log(data);				
+//			});
 
 //https://www.youtube.com/watch?v=urfyp-r255A
 //cdn.jsdeliv.net/npm/world-atlas@2/countries-110m.json		
@@ -27,20 +29,22 @@ var svg = d3.select('body').append("div")
         .attr('width',width)
 	.attr('height',height);
 var tooltip = d3.select("div.tooltip");
+		
+d3.json('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json')
+  .then(data => {
 
-Promise.all([worldmap, econdata]).then(function(values) {
-
-   // var countries = topojson.feature(data, data.objects.countries).features; 
+   var countries = topojson.feature(data, data.objects.countries).features; 
    
    svg.selectAll('path')
-	   .data(values[0].features)
+	   .data(countries)
 	   .enter().append('path')
 	   .attr('class','country')
 	   .attr('fill', 'lightgrey')
 	   .attr('stroke', 'black') 
 	   .attr('stroke-width', '1')
 	   .attr('d', path)
-	   //.attr('fake', d=> console.log(d.properties.name))
+	   .attr('fake', d=> console.log(d.properties.name))
+	   /* Could replace with mouseover, mouseout, see www.youtube.com watch?v=aNbgrqRuoiE */
     .on("mouseover", function(d,i) {
       console.log("mouseover  ",d.properties.name);	   
       d3.select(this).attr("fill","orange").attr("stroke-width",2);
@@ -54,30 +58,7 @@ Promise.all([worldmap, econdata]).then(function(values) {
      })
      .on("mouseout",function(d,i){
          d3.select(this).attr("fill","lightgrey").attr("stroke-width",1);
-         tooltip.classed("hidden", true),
-      })
-	
-	// draw points
-    svg.selectAll("circle")
-        .data(values[1])
-        .enter()
-        .append("circle")
-        .attr("class","circles")
-        .attr("cx", function(d) {return projection([d.Longitude, d.Lattitude])[0];})
-        .attr("cy", function(d) {return projection([d.Longitude, d.Lattitude])[1];})
-        .attr("r", "1px"),
-    // add labels
-     svg.selectAll("text")
-        .data(values[1])
-        .enter()
-        .append("text")
-        .text(function(d) {
-            return d.DIGiniIndex;
-            })
-        .attr("x", function(d) {return projection([d.Longitude, d.Lattitude])[0] + 5;})
-        .attr("y", function(d) {return projection([d.Longitude, d.Lattitude])[1] + 15;})
-        .attr("class","labels");
-});	   
-	   /* Could replace with mouseover, mouseout, see www.youtube.com watch?v=aNbgrqRuoiE */
+         tooltip.classed("hidden", true);
+      });
 
- })
+})
