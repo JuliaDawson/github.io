011a8884c1bae059126e399e57111aefe886af00
JuliaDawson <julia.dawson80@gmail.com>
2021-08-01 09:34:02 -0500

    Update juliaEd_CS416.js

--- juliaEd_CS416.js
+++ juliaEd_CS416.js
@@ -116,6 +116,114 @@ function opPage3(op)
 /*============================================================================================================*/
 /*============================================================================================================*/
 /*============================================================================================================*/
+/* PAGE 1 START  Big Gini Map                                                                                         */
+/*------------------------------------------------------------------------------------------------------------*/
+*============================================================================================================*/
+/* start of Map Chart Big                                                                                          */
+/*------------------------------------------------------------------------------------------------------------*/
+function dsMapChartBig() {
+var width = 1000;
+var height = 500;
+
+var projection = d3.geoMercator().translate([width/2,height/2]).scale(90);
+var path = d3.geoPath().projection(projection);
+
+//Read Topo file and CSV containing economic information
+//var worldmap = d3.json("https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json");
+var worldmap = d3.json("countries-110m-edited_wout_Antarctica.json");
+
+Promise.all([worldmap]).then(values => {
+
+	
+d3.select("#mapChartBigTitle")
+        .attr("x", (width / 2))             
+        .attr("y", 15)
+        .attr("text-anchor", "middle")  
+	.attr("class","title")	 
+        .text("World Map with Gini Index (click, zoom)");
+
+d3.select("#mapChartBig")
+	.append("svg:svg")
+	.attr("width", width)           //set the width and height of our visualization (these will be attributes of the <svg> tag
+	.attr("height", height)
+	.text("World Map with Gini Index (click, zoom)")
+	.append("svg:g")                //make a group to hold our map chart
+	.attr("transform", "translate(10,100)");
+
+	
+ var tooltip = d3.select("div.tooltip");	
+ d3.select("#mapChartBigTT");		
+ var countries = topojson.feature(values[0], values[0].objects.countries).features; 
+  
+d3.select("#mapChartBigPath")	
+        .selectAll('path')
+	.data(countries)
+	.enter().append('path')
+	.attr('class','country')
+	 //.attr('fill', 'lightgrey')
+	.attr("fill", function(d) { 
+             //console.log("d", d)
+             //console.log("giniIndex", d.properties.giniindex)
+             var col =  d3.interpolateBlues((d.properties.giniindex - 20) / 44); 
+             //console.log("col", col)
+             if (col) {
+               //console.log("found col", col, "for d", d)
+               return col
+             } else {
+               return 'white'
+             }
+           })
+	   .attr('stroke', 'black') 
+	   .attr('stroke-width', '1')
+	   .attr('d', path)
+	   //.attr('fake', d=> console.log(d.properties.name))
+    .on("mouseover", function(d,i) {
+      //console.log("mouseover  ",d.properties.name);	   
+      //console.log("mouseover  ",d.properties.region);	   
+      //console.log("mouseover  ",d.properties.giniindex);	   
+      d3.select(this)
+	      .attr("stroke","black").attr("stroke-width",4);
+      //      .attr("fill","orange").attr("stroke-width",2);
+      //return tooltip.style("hidden", false).html("Country: " + d.properties.name + d.properties.region  + d.properties.giniindex)
+      return tooltip.style("hidden", false)
+	.html( "<b>" + d.properties.name + "</b>"  
+	      + "<br>"  + d.properties.region  
+	      + "<br>" +  "GiniIndex: "  + "<b>" + d.properties.giniindex  + "</b>" );
+      })
+    .on("mousemove",function(d){
+       tooltip.classed("hidden", false)
+               .style("top", (d3.event.pageY) + "px")
+               .style("left", (d3.event.pageX + 10) + "px")
+               //.html("Country: " + d.properties.name + d.properties.region + d.properties.giniindex)
+	.html( "<b>" + d.properties.name + "</b>"  
+	      + "<br>"  + d.properties.region  
+	      + "<br>" +  "GiniIndex: "  + "<b>" + d.properties.giniindex  + "</b>" );
+     })	
+     .on("mouseout",function(d,i){
+         d3.select(this)
+	 .attr("stroke","black")
+	 .attr("stroke-width",1);
+         tooltip.classed("hidden", true);
+	 console.log("Mouseout: " + this);  
+      })
+})
+}	
+/*------------------------------------------------------------------------------------------------------------*/	    
+/* End of Map Chart Big                                                                                         */
+/*============================================================================================================*/
+
+/*============================================================================================================*/
+/* ...Run dsMapChartBig now... 
+/*============================================================================================================*//*------------------------------------------------------------------------------------------------------------*/
+/* PAGE 1 END  Big Gini Map                                                                                         */
+/*============================================================================================================*/
+/*============================================================================================================*/
+/*============================================================================================================*/
+
+
+/*============================================================================================================*/
+/*============================================================================================================*/
+/*============================================================================================================*/
 /* PAGE 2 START  WI                                                                                         */
 /*------------------------------------------------------------------------------------------------------------*/
 
