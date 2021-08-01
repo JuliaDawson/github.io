23953cfce0f553f44eeda1b2630b1f7296037be2
JuliaDawson <julia.dawson80@gmail.com>
2021-08-01 14:09:53 -0500

    Update juliaEd_CS416.js

--- juliaEd_CS416.js
+++ juliaEd_CS416.js
@@ -9,7 +9,7 @@ var path = d3.geoPath().projection(projection);
 
 //Read Topo file and CSV containing economic information
 //var worldmap = d3.json("https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json");
-var worldmap = await d3.json("countries-110m-edited_wout_Antarctica.json");
+const worldmap = await d3.json("countries-110m-edited_wout_Antarctica.json");
 var countries = topojson.feature(worldmap, worldmap.objects.countries).features; 
 
 //Promise.all([worldmap]).then(values => {
