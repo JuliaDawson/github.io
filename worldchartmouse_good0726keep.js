189d5206a771c63578f1dc8c07e23ba4cae7a07b
JuliaDawson <julia.dawson80@gmail.com>
2021-08-01 06:44:59 -0500

    Update worldchartmouse_good0726keep.js

--- worldchartmouse_good0726keep.js
+++ worldchartmouse_good0726keep.js
@@ -1,51 +1,53 @@
+//Set the initial region value
+let regionGl = '';
+let regionName = '';	
+let priorregionGl = '';	
+let currDataBarChart = [];	
+
+/*============================================================================================================*/
+/* start of Map Chart Big                                                                                          */
+/*------------------------------------------------------------------------------------------------------------*/
+function dsMapChartBig() {
 var width = 900;
 var height = 400;
-var data = [4,8,15,16,23,42];
 
-d3.selectAll('svg > g > *').remove(); 
-
-var projection = d3.geoMercator().translate([width/2,height/2]).scale(140);
+var projection = d3.geoMercator().translate([width/2,height/2]).scale(55);
 var path = d3.geoPath().projection(projection);
-
-//var color = d3.scaleThreshold()
-//    .domain(d3.range(0, 70))
-//    .range(d3.interpolateBlues(20));
 
 //Read Topo file and CSV containing economic information
 //var worldmap = d3.json("https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json");
 var worldmap = d3.json("countries-110m-edited_wout_Antarctica.json");
-var econcsv = d3.csv("MatchTopo_Distribution_of_income_Shared_Prosperity.csv", function(data) {
-    return {	
-            countryName: data.DISPCountry,
-	    region:     data.DISPRegion,
-	    giniIndex: +data.DIGiniIndex
-    }	    
-});					        
-console.log("econcsv: " + econcsv);
 
-Promise.all([worldmap, econcsv]).then(values => {
+Promise.all([worldmap]).then(values => {
+
 	
-	//console.log("values[0]: " + values[0]);
-	//console.log("values[1][0]: " + values[1][0]);
+d3.select("#mapChartBigTitle")
+        .attr("x", (width / 2))             
+        .attr("y", 15)
+        .attr("text-anchor", "middle")  
+	.attr("class","title")	 
+        .text("World Map with Gini Index (click, zoom)");
 
-	var svg = d3.select('body').append("div")
-		.append('svg')
-        	.attr('width',width)
-		.attr('height',height);
+d3.select("#mapChartBig")
+	.append("svg:svg")
+	.attr("width", width)           //set the width and height of our visualization (these will be attributes of the <svg> tag
+	.attr("height", height)
+	.text("World Map with Gini Index (click, zoom)")
+	.append("svg:g")                //make a group to hold our map chart
+	.attr("transform", "translate(10,100)");
+
 	
-	var tooltip = d3.select("div.tooltip");
-		
-// d3.json('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json')
-//  .then(data => {
-
-   var countries = topojson.feature(values[0], values[0].objects.countries).features; 
-   
-   svg.selectAll('path')
-	   .data(countries)
-	   .enter().append('path')
-	   .attr('class','country')
-	   //.attr('fill', 'lightgrey')
-	   .attr("fill", function(d) { 
+ var tooltip = d3.select("div.tooltip");	
+ d3.select("#mapChartBigTT");		
+ var countries = topojson.feature(values[0], values[0].objects.countries).features; 
+  
+d3.select("#mapChartBigPath")	
+        .selectAll('path')
+	.data(countries)
+	.enter().append('path')
+	.attr('class','country')
+	 //.attr('fill', 'lightgrey')
+	.attr("fill", function(d) { 
              //console.log("d", d)
              //console.log("giniIndex", d.properties.giniindex)
              var col =  d3.interpolateBlues((d.properties.giniindex - 20) / 44); 
@@ -70,28 +72,57 @@ Promise.all([worldmap, econcsv]).then(values => {
       //      .attr("fill","orange").attr("stroke-width",2);
       //return tooltip.style("hidden", false).html("Country: " + d.properties.name + d.properties.region  + d.properties.giniindex)
       return tooltip.style("hidden", false)
-	.html("Country: " + d.properties.name  + "<br>" + "Region: " + d.properties.region 
-      			 + "<br>" + "GiniIndex: " + d.properties.giniindex);
+	.html( "<b>" + d.properties.name + "</b>"  
+	      + "<br>"  + d.properties.region  
+	      + "<br>" +  "GiniIndex: "  + "<b>" + d.properties.giniindex  + "</b>" );
       })
     .on("mousemove",function(d){
        tooltip.classed("hidden", false)
                .style("top", (d3.event.pageY) + "px")
                .style("left", (d3.event.pageX + 10) + "px")
                //.html("Country: " + d.properties.name + d.properties.region + d.properties.giniindex)
-	.html("Country: "  + "<b>" + d.properties.name  + "</b>"   + "<br>" + "Region: "  + "<b>" + d.properties.region  + "</b>"  
-      			 + "<br>" + "GiniIndex: "  + "<b>" + d.properties.giniindex  + "</b>" );
+	.html( "<b>" + d.properties.name + "</b>"  
+	      + "<br>"  + d.properties.region  
+	      + "<br>" +  "GiniIndex: "  + "<b>" + d.properties.giniindex  + "</b>" );
      })	
      .on("mouseout",function(d,i){
          d3.select(this)
 	 .attr("stroke","black")
 	 .attr("stroke-width",1);
          tooltip.classed("hidden", true);
+	 console.log("Mouseout: " + this);  
       })
+     .on("click",function(d,i){
+	 console.log("Click: ", d.properties.region, regionGl); 
+	 if (regionGl != d.properties.region) {
+	     console.log("Region changed from:", regionGl, " To:", d.properties.region);	 
+             regionGl = d.properties.region;
+		 
+             currDataBarChart = dataBarChosen(regionGl); //Refine data to clicked Region
+		 
+	     if (regionGl == "South/Latin America" || regionGl == "North America") {
+		regionName = "North and South/Latin America";     
+	     }
+             dsScattChartSP1(regionGl); //refresh Scatter Chart 1
+             dsScattChartSP2(regionGl); //refresh Scatter Chart 1
+	     priorregionGl = regionGl; 	 
+	 } 
+      })
+})
+/*------------------------------------------------------------------------------------------------------------*/	    
+/* End of Map Chart Big                                                                                         */
+/*============================================================================================================*/
 
-});
+/*============================================================================================================*/
+/* ...Run dsMapChartBig now... 
+/*============================================================================================================*/
+  
+    dsMapChartBig(); //mapChart will cause barChart and lineChart to refresh if necessary
+
 async function init() 
 {
 	// set up locations
+	d3.select('#mapChartBig').attr("transform",   "translate(10,0)");	
 	//d3.select('#mapChartWI').attr("transform",   "translate(10,0)");	
 	//d3.select('#scattChartSP1').attr("transform",  "translate(400,15)");	
 	//d3.select('#scattChartSP2').attr("transform",  "translate(400,250)");	
